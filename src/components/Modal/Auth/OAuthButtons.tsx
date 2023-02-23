3939298bbb23120866916e93a7cc02a58f6e25d9
clearyed <eddiejcleary@gmail.com>
2023-02-23 12:11:09 -0700

    Connected Google sign in and errors

--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -1,10 +1,19 @@
-import { Flex, Button, Image } from "@chakra-ui/react";
+import { Flex, Button, Image, Text } from "@chakra-ui/react";
 import React from "react";
+import { auth } from "@/firebase/clientApp";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 
 const OAuthButtons: React.FC = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
   return (
     <Flex direction="column" width="100%" mb={4}>
-      <Button variant="oauth" mb={2}>
+      <Button
+        variant="oauth"
+        mb={2}
+        isLoading={loading}
+        onClick={() => signInWithGoogle()}
+      >
         <Image
           src="/images/googleLogo.png"
           alt="Google logo"
@@ -14,6 +23,7 @@ const OAuthButtons: React.FC = () => {
         Continue with Google
       </Button>
       <Button variant="oauth">Some Other Provider</Button>
+      {error && <Text>{error.message}</Text>}
     </Flex>
   );
 };
