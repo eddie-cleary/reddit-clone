90f384fe07d44c554e9834a92b69d708398e1164
clearyed <eddiejcleary@gmail.com>
2023-02-23 12:11:38 -0700

    Added current user from firebase

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,8 +2,12 @@ import React from "react";
 import { Image, Flex } from "@chakra-ui/react";
 import SearchInput from "./SearchInput";
 import RightContent from "./RightContent/RightContent";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/clientApp";
 
 const Navbar: React.FC = () => {
+  const [user, loading, error] = useAuthState(auth);
+
   return (
     <Flex bg="white" height="44px" padding="6px 12px">
       <Flex align="center">
@@ -24,7 +28,7 @@ const Navbar: React.FC = () => {
       </Flex>
       {/* <Directory /> */}
       <SearchInput />
-      <RightContent />
+      <RightContent user={user} />
     </Flex>
   );
 };
