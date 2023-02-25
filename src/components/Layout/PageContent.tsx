d083c9489c5be300f1cb07d8173e8f2136b932e3
clearyed <eddiejcleary@gmail.com>
2023-02-25 15:37:58 -0700

    Created Page Layout component

---
+++ src/components/Layout/PageContent.tsx
@@ -0,0 +1,41 @@
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+import { PropsWithChildren } from "react";
+
+type PageContentProps = {
+  children?: React.ReactNode[];
+};
+
+const PageContent: React.FC<PageContentProps> = (props) => {
+  console.log("the children passed in are ", props.children);
+  return (
+    <Flex justify="center" p="16px 8px" border="1px solid red">
+      <Flex
+        width="95%"
+        justify="center"
+        maxWidth="860px"
+        border="1px solid green"
+      >
+        {/* LHS */}
+        <Flex
+          direction="column"
+          width={{ base: "100%", md: "65%" }}
+          mr={{ base: 0, md: 6 }}
+          border="1px solid blue"
+        >
+          {props.children && props.children[0]}
+        </Flex>
+        {/* RHS */}
+        <Flex
+          direction="column"
+          display={{ base: "none", md: "flex" }}
+          flexGrow={1}
+          border="1px solid orange"
+        >
+          {props.children && props.children[1]}
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+};
+export default PageContent;
