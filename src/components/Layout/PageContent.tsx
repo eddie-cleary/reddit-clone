1e4602b1271a8e6382b65f1f151f745352762d6b
clearyed <eddiejcleary@gmail.com>
2023-02-25 18:09:46 -0700

    Removed dev borders

--- src/components/Layout/PageContent.tsx
+++ src/components/Layout/PageContent.tsx
@@ -8,19 +8,13 @@ type PageContentProps = {
 
 const PageContent: React.FC<PageContentProps> = (props) => {
   return (
-    <Flex justify="center" p="16px 8px" border="1px solid red">
-      <Flex
-        width="95%"
-        justify="center"
-        maxWidth="860px"
-        border="1px solid green"
-      >
+    <Flex justify="center" p="16px 8px">
+      <Flex width="95%" justify="center" maxWidth="860px">
         {/* LHS */}
         <Flex
           direction="column"
           width={{ base: "100%", md: "65%" }}
           mr={{ base: 0, md: 6 }}
-          border="1px solid blue"
         >
           {props.children && props.children[0]}
         </Flex>
@@ -29,7 +23,6 @@ const PageContent: React.FC<PageContentProps> = (props) => {
           direction="column"
           display={{ base: "none", md: "flex" }}
           flexGrow={1}
-          border="1px solid orange"
         >
           {props.children && props.children[1]}
         </Flex>
