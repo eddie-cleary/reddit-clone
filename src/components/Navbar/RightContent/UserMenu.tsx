fde79c8adf18fb2d2bcca2da5983635d6184ed51
clearyed <eddiejcleary@gmail.com>
2023-02-25 17:16:48 -0700

    Updated logout to also reset global comm state

--- src/components/Navbar/RightContent/UserMenu.tsx
+++ src/components/Navbar/RightContent/UserMenu.tsx
@@ -17,15 +17,22 @@ import { IoSparkles } from "react-icons/io5";
 import { CgProfile } from "react-icons/cg";
 import { MdOutlineLogin } from "react-icons/md";
 import { auth } from "@/firebase/clientApp";
-import { useSetRecoilState } from "recoil";
+import { useResetRecoilState, useSetRecoilState } from "recoil";
 import { authModalState } from "@/atoms/authModalAtom";
+import { communityState } from "@/atoms/communitiesAtom";
 
 type UserMenuProps = {
   user?: User | null;
 };
 
 const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
+  const resetCommunityState = useResetRecoilState(communityState);
   const setAuthModalState = useSetRecoilState(authModalState);
+
+  const logout = async () => {
+    await signOut(auth);
+    resetCommunityState();
+  };
 
   return (
     <Menu>
@@ -92,7 +99,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
                 bg: "blue.500",
                 color: "white",
               }}
-              onClick={() => signOut(auth)}
+              onClick={logout}
             >
               <Flex align="center">
                 <Icon as={MdOutlineLogin} fontSize="20pt" mr={2} />
