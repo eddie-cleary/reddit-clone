f5d82418516b7b1864252583990b2e7b6a619b73
clearyed <eddiejcleary@gmail.com>
2023-02-25 21:45:54 -0700

    Created TextInputs component

---
+++ src/components/Posts/PostForm/TextInputs.tsx
@@ -0,0 +1,64 @@
+import { Stack, Input, Textarea, Flex, Button } from "@chakra-ui/react";
+import React from "react";
+
+type TextInputsProps = {
+  textInputs: {
+    title: string;
+    body: string;
+  };
+  onChange: (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+  handleCreatePost: () => void;
+  loading: boolean;
+};
+
+const TextInputs: React.FC<TextInputsProps> = (props) => {
+  return (
+    <Stack spacing={3} width="100%">
+      <Input
+        name="title"
+        // value={}
+        onChange={props.onChange}
+        fontSize="10pt"
+        borderRadius={4}
+        placeholder="Title"
+        _placeholder={{ color: "gray.500" }}
+        _focus={{
+          outline: "none",
+          bg: "white",
+          border: "1px solid",
+          borderColor: "black",
+        }}
+      />
+      <Textarea
+        name="body"
+        fontSize="10pt"
+        borderRadius={4}
+        height="100px"
+        value={props.textInputs.body}
+        onChange={props.onChange}
+        placeholder="Text (optional)"
+        _placeholder={{ color: "gray.500" }}
+        _focus={{
+          outline: "none",
+          bg: "white",
+          border: "1px solid",
+          borderColor: "black",
+        }}
+      />
+      <Flex justify="flex-end">
+        <Button
+          height="34px"
+          px="30px"
+          disabled={!props.textInputs.title}
+          onClick={props.handleCreatePost}
+          isLoading={props.loading}
+        >
+          Post
+        </Button>
+      </Flex>
+    </Stack>
+  );
+};
+export default TextInputs;
