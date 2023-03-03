cbb8bff9c189a61c3360def4dbfd185339fb9128
clearyed <eddiejcleary@gmail.com>
2023-03-02 17:12:20 -0700

    Updated NewPostForm, only show if user exists

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -2,15 +2,19 @@ import PageContent from "@/components/Layout/PageContent";
 import NewPostForm from "@/components/Posts/NewPostForm";
 import { Box, Text } from "@chakra-ui/react";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/clientApp";
 
 const SubmitPostPage: React.FC = () => {
+  const [user] = useAuthState(auth);
+
   return (
     <PageContent>
       <>
         <Box py="14px" borderBottom="1px solid white">
           <Text>Create a post</Text>
         </Box>
-        <NewPostForm />
+        {user && <NewPostForm user={user} />} 
       </>
       <>{/* AboutComponent */}</>
     </PageContent>
