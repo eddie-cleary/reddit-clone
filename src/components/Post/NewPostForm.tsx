7c65bf6107f2924288c5486dc5bb17fda37b9418
clearyed <eddiejcleary@gmail.com>
2023-03-02 19:27:43 -0700

    Created NewPostForm

---
+++ src/components/Post/NewPostForm.tsx
@@ -0,0 +1,170 @@
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Flex,
+  Icon,
+  Text,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { BiPoll } from "react-icons/bi";
+import { BsLink45Deg, BsMic } from "react-icons/bs";
+import { IoDocumentText, IoImageOutline } from "react-icons/io5";
+import { AiFillCloseCircle } from "react-icons/ai";
+import TabItem from "./TabItem";
+import TextInputs from "./PostForm/TextInputs";
+import ImageUpload from "./PostForm/ImageUpload";
+import { Post } from "@/atoms/postsAtom";
+import { User } from "firebase/auth";
+import { useRouter } from "next/router";
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  Timestamp,
+  updateDoc,
+} from "firebase/firestore";
+import { firestore, storage } from "@/firebase/clientApp";
+import { getDownloadURL, ref, uploadString } from "firebase/storage";
+
+type NewPostFormProps = {
+  user: User;
+};
+
+const formTabs: TabItemType[] = [
+  {
+    title: "Post",
+    icon: IoDocumentText,
+  },
+  {
+    title: "Images & Video",
+    icon: IoImageOutline,
+  },
+  {
+    title: "Link",
+    icon: BsLink45Deg,
+  },
+  {
+    title: "Poll",
+    icon: BiPoll,
+  },
+  {
+    title: "Talk",
+    icon: BsMic,
+  },
+];
+
+export type TabItemType = {
+  title: string;
+  icon: typeof Icon.arguments;
+};
+
+const NewPostForm: React.FC<NewPostFormProps> = ({ user }) => {
+  const router = useRouter();
+  const [selectedTab, setSelectedTab] = useState(formTabs[0].title);
+  const [textInputs, setTextInputs] = useState({ title: "", body: "" });
+  const [selectedFile, setSelectedFile] = useState<string>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleCreatePost = async () => {
+    const { communityId } = router.query;
+
+    try {
+      setLoading(true);
+      // store the post in our db collection 'posts'
+      const postDocRef = await addDoc(collection(firestore, "posts"), {
+        communityId,
+        creatorId: user?.uid,
+        creatorDisplayName: user.email!.split("@")[0],
+        title: textInputs.title,
+        body: textInputs.body,
+        numberOfComments: 0,
+        voteStatus: 0,
+        createdAt: serverTimestamp() as Timestamp,
+      });
+
+      // if the user included an image, store it in firebase storage
+      if (selectedFile) {
+        const imageRef = ref(storage, `posts/${postDocRef.id}/image`);
+        await uploadString(imageRef, selectedFile, "data_url");
+        const downloadURL = await getDownloadURL(imageRef);
+        // update post doc with image url
+        await updateDoc(postDocRef, {
+          imageURL: downloadURL,
+        });
+        // redirect user to community page
+        router.back();
+      }
+    } catch (error: any) {
+      console.error(error.message);
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onSelectImage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const reader = new FileReader();
+
+    if (event.target.files?.[0]) {
+      reader.readAsDataURL(event.target.files[0]);
+    }
+
+    reader.onload = (readerEvent) => {
+      if (readerEvent.target?.result) {
+        setSelectedFile(readerEvent.target.result as string);
+      }
+    };
+  };
+
+  const onTextChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const {
+      target: { name, value },
+    } = event;
+    setTextInputs((prev) => ({ ...prev, [name]: value }));
+  };
+
+  return (
+    <Flex direction="column" bg="white" borderRadius={4} mt={2}>
+      <Flex width="100%">
+        {formTabs.map((item) => (
+          <TabItem
+            key={item.title}
+            item={item}
+            selected={item.title === selectedTab}
+            setSelectedTab={setSelectedTab}
+          />
+        ))}
+      </Flex>
+      <Flex p={4}>
+        {selectedTab === "Post" && (
+          <TextInputs
+            textInputs={textInputs}
+            handleCreatePost={handleCreatePost}
+            onChange={onTextChange}
+            loading={loading}
+          />
+        )}
+        {selectedTab === "Images & Video" && (
+          <ImageUpload
+            selectedFile={selectedFile}
+            onSelectImage={onSelectImage}
+            setSelectedTab={setSelectedTab}
+            setSelectedFile={setSelectedFile}
+          />
+        )}
+      </Flex>
+      {error && (
+        <Alert status="error">
+          <AlertIcon />
+          <Text mr={2}>Error creating post</Text>
+        </Alert>
+      )}
+    </Flex>
+  );
+};
+export default NewPostForm;
