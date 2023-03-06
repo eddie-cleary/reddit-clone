ff15731a89da7eb16b22d860a94b685f0c3a9bfe
clearyed <eddiejcleary@gmail.com>
2023-03-05 17:28:27 -0700

    Added fetching of post if not in state

--- src/pages/r/[communityId]/comments/[pid].tsx
+++ src/pages/r/[communityId]/comments/[pid].tsx
@@ -1,13 +1,42 @@
 import PageContent from "@/components/Layout/PageContent";
 import PostItem from "@/components/Post/PostItem";
 import usePosts from "@/hooks/usePosts";
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "@/firebase/clientApp";
+import { auth, firestore } from "@/firebase/clientApp";
+import { useRouter } from "next/router";
+import { doc, getDoc } from "firebase/firestore";
+import { Post } from "@/atoms/postsAtom";
 
 const PostPage: React.FC = () => {
-  const { postStateValue, onDeletePost, onVote } = usePosts();
+  const { postStateValue, setPostStateValue, onDeletePost, onVote } =
+    usePosts();
   const [user] = useAuthState(auth);
+  const router = useRouter();
+
+  const fetchPost = useCallback(
+    async (postId: string) => {
+      try {
+        const postDocRef = doc(firestore, `posts`, postId);
+        const postDoc = await getDoc(postDocRef);
+        setPostStateValue((prev) => ({
+          ...prev,
+          selectedPost: { id: postDoc.id, ...postDoc.data() } as Post,
+        }));
+      } catch (error: any) {
+        console.error(error.message);
+      }
+    },
+    [setPostStateValue]
+  );
+
+  useEffect(() => {
+    const { pid } = router.query;
+
+    if (pid && !postStateValue.selectedPost) {
+      fetchPost(pid as string);
+    }
+  }, [postStateValue.selectedPost, fetchPost, router.query]);
 
   return (
     <PageContent>
@@ -33,3 +62,6 @@ const PostPage: React.FC = () => {
   );
 };
 export default PostPage;
+function useCallBack(arg0: (postId: string) => Promise<void>, arg1: never[]) {
+  throw new Error("Function not implemented.");
+}
