e8677e3163e28bbeddeec58ec914bdf591f395b5
clearyed <eddiejcleary@gmail.com>
2023-03-10 16:58:52 -0700

    Added onSelectMenuItem onClick function

--- src/components/Navbar/Directory/MenuListItem.tsx
+++ src/components/Navbar/Directory/MenuListItem.tsx
@@ -1,3 +1,4 @@
+import useDirectory from "@/hooks/useDirectory";
 import { Flex, Icon, Image, MenuItem } from "@chakra-ui/react";
 import React from "react";
 import { IconType } from "react-icons";
@@ -10,27 +11,39 @@ type MenuListItemProps = {
   imageURL?: string;
 };
 
-const MenuListItem: React.FC<MenuListItemProps> = (props) => (
-  <MenuItem
-    width="100%"
-    fontSize="10pt"
-    _hover={{ bg: "gray.100" }}
-    onClick={() => {}}
-  >
-    <Flex align="center">
-      {props.imageURL ? (
-        <Image
-          alt={props.displayText}
-          src={props.imageURL}
-          borderRadius="full"
-          boxSize="18px"
-          mr={2}
-        />
-      ) : (
-        <Icon as={props.icon} fontSize={20} mr={2} color={props.iconColor} />
-      )}
-      {props.displayText}
-    </Flex>
-  </MenuItem>
-);
+const MenuListItem: React.FC<MenuListItemProps> = (props) => {
+  const { onSelectMenuItem } = useDirectory();
+
+  return (
+    <MenuItem
+      width="100%"
+      fontSize="10pt"
+      _hover={{ bg: "gray.100" }}
+      onClick={() =>
+        onSelectMenuItem({
+          displayText: props.displayText,
+          link: props.link,
+          icon: props.icon,
+          iconColor: props.iconColor,
+          imageURL: props.imageURL,
+        })
+      }
+    >
+      <Flex align="center">
+        {props.imageURL ? (
+          <Image
+            alt={props.displayText}
+            src={props.imageURL}
+            borderRadius="full"
+            boxSize="18px"
+            mr={2}
+          />
+        ) : (
+          <Icon as={props.icon} fontSize={20} mr={2} color={props.iconColor} />
+        )}
+        {props.displayText}
+      </Flex>
+    </MenuItem>
+  );
+};
 export default MenuListItem;
