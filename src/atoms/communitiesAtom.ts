bfa194511805fa95ad51ccbfcbf3f2dfc630c334
clearyed <eddiejcleary@gmail.com>
2023-03-11 19:27:37 -0700

    Name change

--- src/atoms/communitiesAtom.ts
+++ src/atoms/communitiesAtom.ts
@@ -20,13 +20,15 @@ export interface CommunitySnippet {
 interface CommunityState {
   mySnippets: CommunitySnippet[];
   currentCommunity?: Community;
+  snippetsFetched: boolean;
 }
 
 const defaultCommunityState: CommunityState = {
   mySnippets: [],
+  snippetsFetched: false,
 };
 
 export const communityState = atom<CommunityState>({
-  key: "communitiesState",
+  key: "communityState",
   default: defaultCommunityState,
 });
